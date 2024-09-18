a07ada0481bbcac4e38e4cf16a2baa61c18f9301
ShoaibAlam107 <shoaib.cap107@gmail.com>
2024-09-19 03:17:28 +0600

    open in new page

--- js/login.js
+++ js/login.js
@@ -9,10 +9,19 @@ document.getElementById('btn-login').addEventListener('click',function(event){
     
     // step-2 prevent default (prevent reloading browser)
     event.preventDefault(); // <----vejal lagbe janar jonno just---->
-    console.log('login button clicked');
+    // console.log('login button clicked');
 
     // step-3 get the phone number
     const phoneNumber = document.getElementById('phn-num').value;
-    console.log(phoneNumber);
+    const passNumber = document.getElementById('pass-number').value;
+    console.log(phoneNumber , passNumber);
+
+    if( phoneNumber === '2024' && passNumber === '2222'){
+        console.log('login')
+        window.location.href = '/home.html';
+    }
+    else{
+        alert('wrong phn or pin')
+    }
 
 })
