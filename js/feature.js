45ae563c18439018c913c52047b2c3fc1dbf246d
ShoaibAlam107 <shoaib.cap107@gmail.com>
2024-09-21 04:40:50 +0600

    features adding in button

---
+++ js/feature.js
@@ -0,0 +1,17 @@
+// show the cash out from.
+
+document.getElementById('show-cash-out')
+.addEventListener('click',function(){
+    console.log('show the cash out from');
+    document.getElementById('cash-out-form').classList.remove('hidden');
+
+    document.getElementById('add-money-form').classList.add('hidden');
+
+
+});
+
+document.getElementById('show-add-money').addEventListener('click',function(){
+    document.getElementById('add-money-form').classList.remove('hidden');
+    document.getElementById('cash-out-form').classList.add('hidden');
+
+});
--- js/home.js
+++ js/home.js
@@ -33,7 +33,7 @@ document.getElementById('btn-add-money')
         const newBalance = currentBalance + inputAddMoney;
         console.log(newBalance);
 
-        // step-6 update balance inn the UI/DOM.
+        // step-6 update balance inn the UI/DOM by add money.
         document.getElementById('main-balance').innerText =  newBalance;
 
      } 
@@ -48,3 +48,52 @@ document.getElementById('btn-add-money')
 });
 
 
+
+
+// Cash out money from the account.
+
+document.getElementById('btn-Cash-out')
+.addEventListener('click', function(event){
+//     // prevent page reload.
+    event.preventDefault();
+    
+    // step-2 get money to be Cash out from the account.
+    const addMoney =document.getElementById('input-Cash-out').value;
+    const  inputAddMoney =parseFloat(addMoney)
+    console.log(inputAddMoney);
+
+    // get pin number provided.
+    const inputPin =document.getElementById('Cash-out-pin-number').value;
+    // console.log(inputPin);
+    
+
+
+    // step-3 verify pin number.
+    // wrong way to verify.
+    if( inputPin === '2222'){
+            console.log('Cash out money from your account');
+
+             //  step-4 get the current balance .(it's not a input field,so it's value provided by {.innerText} not {.value}.)
+
+        const mainBalance = document.getElementById('main-balance').innerText;
+        const currentBalance = parseFloat(mainBalance)
+        console.log(currentBalance);
+
+        // step-5 Cash out money from main balance. 
+
+        const newBalance = currentBalance - inputAddMoney;
+        console.log(newBalance);
+
+        // step-6 update balance in the UI/DOM by Cash out...
+        document.getElementById('main-balance').innerText =  newBalance;
+
+     } 
+     else{
+        alert('failed add money! plz try again')
+     }
+
+   
+
+
+ 
+});
