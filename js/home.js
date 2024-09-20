f4ceb531d083df864c1bbd2aa89683bb704a7b7f
ShoaibAlam107 <shoaib.cap107@gmail.com>
2024-09-21 03:39:47 +0600

    Updating balance

--- js/home.js
+++ js/home.js
@@ -7,12 +7,44 @@ document.getElementById('btn-add-money')
     event.preventDefault();
     
     // step-2 get money to be added to the account.
-    const addMony =document.getElementById('input-add-money').value;
-    console.log(addMony);
+    const addMoney =document.getElementById('input-add-money').value;
+    const  inputAddMoney =parseFloat(addMoney)
+    console.log(inputAddMoney);
+
     // get pin number provided.
     const inputPin =document.getElementById('input-pin-number').value;
-    console.log(inputPin);
+    // console.log(inputPin);
+    
 
+
+    // step-3 verify pin number.
+    // wrong way to verify.
+    if( inputPin === '2222'){
+            // console.log('adding money to your account');
+
+             //  step-4 get the current balance .(it's not a input field,so it's value provided by {.innerText} not {.value}).
+
+        const mainBalance = document.getElementById('main-balance').innerText;
+        const currentBalance = parseFloat(mainBalance)
+        console.log(currentBalance);
+
+        // step-5 add money with main balance. 
+
+        const newBalance = currentBalance + inputAddMoney;
+        console.log(newBalance);
+
+        // step-6 update balance inn the UI/DOM.
+        document.getElementById('main-balance').innerText =  newBalance;
+
+     } 
+     else{
+        alert('failed add money! plz try again')
+     }
+
+   
+
+
+ 
 });
 
 
